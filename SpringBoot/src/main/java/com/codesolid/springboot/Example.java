7f63fede703933b3a4d89881d20b0a1c302e771c
JohnLockwood <elitepropertiesbroker@gmail.com>
2014-09-06 19:07:29 -0700

    Step 3, Hello World, non-random password

--- SpringBoot/src/main/java/com/codesolid/springboot/Example.java
+++ SpringBoot/src/main/java/com/codesolid/springboot/Example.java
@@ -2,8 +2,14 @@ package com.codesolid.springboot;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.CommandLineRunner;
+
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,6 +26,7 @@ public class Example {
     }
     
     public static void main(String[] args) throws Exception {
+        System.setProperty("security.user.password", "DoubleSecretProbation");
         SpringApplication.run(Example.class, args);
     }
 
